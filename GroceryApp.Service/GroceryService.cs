caa77e99d30c8b7c38d97027d0cae519209fc5cb
Taylor <taylorfulner@gmail.com>
2021-06-03 15:24:27 -0400

    added get grocerybyid

--- GroceryApp.Service/GroceryService.cs
+++ GroceryApp.Service/GroceryService.cs
@@ -20,9 +20,41 @@ namespace GroceryApp.Service
         {
             var entity = new GroceryStores()
             {
+                Name = model.Name,
+                Address = model.Address,
+                Website = model.Website,
+                PhoneNumber = model.PhoneNumber,
+                App = model.App,
+                Hours = model.Hours
+            };
 
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.GroceryStores.Add(entity);
+                return ctx.SaveChanges() == 1;
             }
         }
+
+        public GroceryDetail GetGroceryStoreyById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx
+                    .GroceryStores
+                    .Single(e => e.GroceryStoreID == id);
+                return new GroceryDetail
+                {
+                    GroceryStoreId = entity.GroceryStoreID,
+                    Name = entity.Name,
+                    Address = entity.Address,
+                    Website = entity.Website,
+                    PhoneNumber = entity.PhoneNumber,
+                    AppContext = entity.App,
+                    Hours = entity.Hours
+                };
+            }
+        }
+
 
     }
 }
